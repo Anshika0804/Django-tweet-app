73437229eadc4df97c97d425c4d498b5b784a7f8
Anshika Rai <anshikacoder10@gmail.com>
2025-07-15 16:12:43 +0530

    Add comment edit and delete feature

--- comments/urls.py
+++ comments/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import add_comment
+from .views import add_comment, edit_comment, delete_comment, comment_list
 
 urlpatterns = [
     path('add/<int:tweet_id>/', add_comment, name='add_comment'),
+    path('<int:tweet_id>/comments/', comment_list, name='comment_list'),
+    path('edit/<int:comment_id>/', edit_comment, name='edit_comment'),
+    path('delete/<int:comment_id>/', delete_comment, name='delete_comment'),
 ]
--- comments/views.py
+++ comments/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from tweet.models import Tweet
 from .forms import CommentForm
+from .models import Comment
 
 # Create your views here.
 @login_required
@@ -14,4 +15,57 @@ def add_comment(request, tweet_id):
             comment.user = request.user
             comment.tweet = tweet
             comment.save()
-    return redirect('tweet_list')       
+    return redirect('tweet_list') 
+
+@login_required
+def comment_list(request, tweet_id):
+    tweet = get_object_or_404(Tweet, id=tweet_id)
+    comments = tweet.comments.all()
+    form = CommentForm()
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.user = request.user
+            new_comment.tweet = tweet
+            new_comment.save()
+            return redirect('comment_list', tweet_id=tweet.id)
+
+    return render(request, 'comments/comment_list.html', {
+        'tweet': tweet,
+        'comments': comments,
+        'form': form,
+    })
+
+
+
+
+@login_required
+def edit_comment(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id)
+
+    if request.user != comment.user:
+        return redirect('tweet_list')  # deny editing if not author
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            form.save()
+            return redirect('tweet_list')
+    else:
+        form = CommentForm(instance=comment)
+
+    return render(request, 'comments/edit_comments.html', {'form': form})
+
+
+@login_required
+def delete_comment(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id)
+
+    if request.user == comment.user:
+        comment.delete()
+
+    return redirect('tweet_list')
+
+
